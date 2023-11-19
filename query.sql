f6da5a5436722d0caf60d7ab4f5d6ef8574f78f8
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-11-18 23:07:25 -0300

    Criacao das tabelas e aarquivo de dump

---
+++ query.sql
@@ -0,0 +1,19 @@
+create table
+    enderecos(
+        id serial primary key,
+        cidade text,
+        logradouro text,
+        bairro text,
+        cep text,
+        numero text default 'sn',
+        data_cadastro date default now()
+    );
+
+create table
+    restaurantes(
+        id serial primary key,
+        nome text not null,
+        horario text not null,
+        foto text,
+        endereco int references enderecos(id)
+    );
