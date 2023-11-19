eddf80740fcbdbc7b4d9e4a9134517312139f241
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-11-19 02:14:21 -0300

    ajustando listar restaurantes

--- src/controladores/restaurante/listarTodosRestaurantes.js
+++ src/controladores/restaurante/listarTodosRestaurantes.js
@@ -3,12 +3,19 @@ const knex = require('../../configuracoes/conexao_database');
 const listarTodosRestaurantes = async (_req, res) => {
     try {
         const restaurantesBd = await knex('restaurantes')
-            .orderBy('id');
-        return res.status(200).json(restaurantesBd)
+            .select(
+                'restaurantes.*',
+                'enderecos.*'
+            )
+            .leftJoin('enderecos', 'enderecos.id', 'restaurantes.endereco')
+            .orderBy('restaurantes.id');
+
+        return res.status(200).json(restaurantesBd);
     } catch (error) {
-        return res.status(500).json({ mensagem: 'Erro interno do servidor', resposta: error.message })
+        return res.status(500).json({ mensagem: 'Erro interno do servidor', resposta: error.message });
     }
 };
+
 
 module.exports = {
     listarTodosRestaurantes
