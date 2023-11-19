7c74927a1dd74ffbc35218b8665bf488f27ba92f
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-11-19 20:19:00 -0300

    Ajustando saida das datas do detalhar restaurante

--- src/intermediaros/restaurantes/intermediario_detalahar_restaurante.js
+++ src/intermediaros/restaurantes/intermediario_detalahar_restaurante.js
@@ -1,17 +1,22 @@
 const knex = require('../../configuracoes/conexao_database');
+const { format } = require('date-fns');
 
 const intermediarioDetalharRestaurante = async (req, res, next) => {
     const { id } = req.params;
     try {
-        const restaurante = await knex('restaurantes')
+        const restaurantesBd = await knex('restaurantes')
             .select('restaurantes.*', 'enderecos.*')
             .leftJoin('enderecos', 'enderecos.id', 'restaurantes.endereco')
             .where('restaurantes.id', id)
             .first();
-        if (!restaurante) {
+        if (!restaurantesBd) {
             return res.status(404).json({ mensagem: 'Restaurante não encontrado com esse ID.' });
         };
-        req.restaurante = restaurante;
+        const restaurantesFormatados = {
+            ...restaurantesBd,
+            data_cadastro: format(restaurantesBd.data_cadastro, 'dd/MM/yyyy')
+        }
+        req.restaurante = restaurantesFormatados;
         next();
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor.', resposta: error.message })
