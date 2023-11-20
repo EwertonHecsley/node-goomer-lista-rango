5314d8aa26290d265abdcf30798add31dd84a3fd
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-11-20 15:53:10 -0300

    ajustes remover promocao do produto

--- src/controladores/promocao/removerPromocaoProduto.js
+++ src/controladores/promocao/removerPromocaoProduto.js
@@ -3,7 +3,7 @@ const knex = require('../../configuracoes/conexao_database');
 const removerPromocaoProduto = async (req, res) => {
     const { id } = req.params;
     try {
-        await knex('produtos').update({ promocao: false }).where({ id });
+        await knex('produtos').update({ promocao: null }).where({ id });
         return res.status(200).json({ mensagem: 'Promocao removida do produto' })
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor.', resposta: error.message })
