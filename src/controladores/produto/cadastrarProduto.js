499367afdde3ae9c9d9bd5079d6c66eef88bcaf1
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-11-20 14:56:44 -0300

    Ajutes cadastrar Produto

--- src/controladores/produto/cadastrarProduto.js
+++ src/controladores/produto/cadastrarProduto.js
@@ -3,13 +3,22 @@ const { uploadImagem } = require('../../configuracoes/conexao_aws');
 
 
 const cadastrarProduto = async (req, res) => {
-    const { nome, preco_produto, categoria_produto_id, promocao } = req.body;
-    const { originalname, buffer, mimetype } = req.file;
+    const { nome, preco_produto, categoria_produto_id } = req.body;
+
     try {
-        const imagem = await uploadImagem(`produtos/${nome}/${originalname}`, buffer, mimetype);
-        const produto = await knex('produtos')
-            .insert({ nome, preco_produto, categoria_produto_id, promocao, foto: imagem.url })
+        let produto = await knex('produtos')
+            .insert({ nome, preco_produto, categoria_produto_id })
             .returning('*');
+
+        if (req.file) {
+            const { originalname, buffer, mimetype } = req.file;
+            const { id } = produto[0]
+
+            const imagem = await uploadImagem(`produtos/${id}/${nome}/${originalname}`, buffer, mimetype);
+
+            produto = await knex('produtos').update({ foto: imagem.url }).where({ id }).returning('*');
+        }
+
         return res.status(201).json({ mensagem: 'Produto cadastrado com sucesso.', produto: produto[0] })
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor.', resposta: error.message })
